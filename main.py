bbafaa534767f6e91a5390e04bf76d71ccfc6def
Marion Robert <107509668+marionrobert@users.noreply.github.com>
2024-05-08 17:17:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,17 +26,6 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
     def to_dict(self):
-        # # Method 1.
-        # dictionary = {}
-        # # Loop through each column in the data record
-        # for column in self.__table__.columns:
-        #     # Create a new dictionary entry;
-        #     # where the key is the name of the column
-        #     # and the value is the value of the column
-        #     dictionary[column.name] = getattr(self, column.name)
-        # return dictionary
-
-        # Method 2. Alternatively use Dictionary Comprehension to do the same thing.
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
@@ -50,19 +39,6 @@ def get_random_cafe():
     if request.method == "GET":
         all_cafes = Cafe.query.all()
         random_cafe = random.choice(all_cafes)
-        # return jsonify(cafe={
-        #     "can_take_calls": random_cafe.can_take_calls,
-        #     "coffee_price": random_cafe.coffee_price,
-        #     "has_sockets": random_cafe.has_sockets,
-        #     "has_toilet": random_cafe.has_toilet,
-        #     "has_wifi": random_cafe.has_wifi,
-        #     "id": random_cafe.id,
-        #     "img_url": random_cafe.img_url,
-        #     "location": random_cafe.location,
-        #     "map_url": random_cafe.map_url,
-        #     "name": random_cafe.name,
-        #     "seats": random_cafe.seats
-        # })
         return jsonify(cafe=random_cafe.to_dict())
 
 
